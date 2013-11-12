88dc6621e6f39de9d44a82de213cdee5a4fa0b32
Steven Mathias <smathias@crowdfavorite.com>
2013-11-12 12:37:59 -0700

    Updated version number and changelog.

--- wp-rebase-data.php
+++ wp-rebase-data.php
@@ -4,7 +4,7 @@
  * Plugin URI: http://github.com/ssmathias/wp-rebase-data
  * Description: Allows a site admin to trigger "save" actions on a variety of data programmatically.
  * Author: Steven Mathias
- * Version: indev
+ * Version: 2.1
  * Author URI: http://github.com/ssmathias/
  **/
 
